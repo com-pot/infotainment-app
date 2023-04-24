e7664c027b36290cfcc19237869838ff957fda49
Toanir <Thoronir42@Gmail.com>
2023-04-24 19:38:27 +0200

    Extract custom spec definition selection into own file
    Fixes #1

---
+++ libs/@typeful/vite-custom/src/selectiveImport.ts
@@ -0,0 +1,27 @@
+
+export function mapImporters<T = unknown>(
+    importers: Record<string, () => Promise<T>>,
+    searchValue: string = '../custom',
+    replaceValue: string = '@custom',
+): Record<string, () => Promise<T>> {
+    const entries = Object.entries(importers)
+        .map(([path, loadFn]) => [path.replace(searchValue, replaceValue), loadFn])
+
+    return Object.fromEntries(entries)
+}
+
+export function selectImporter<T>(importers: Record<string, () => Promise<T>>, name: string): () => Promise<T> {
+    const importFn = importers[name]
+        || importers[name + '.js']
+        || importers[name + '.ts']
+
+    if (!importFn) {
+        return () => Promise.reject(`App spec '${name}' not found`)
+    }
+    return importFn
+}
+
+export async function importAsObj<T extends object>(importer: () => Promise<T>) {
+    const module = await importer()
+    return {...module}
+}
--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,13 @@ import conGameItPanelModule from '@custom/com-pot/con-game/conGame.itPanelModule
 import i18nPlugin from '@custom/com-pot/i18n/i18n.plugin'
 
 import "./sass/infotainment.scss"
+import { importAsObj, mapImporters, selectImporter } from '@typeful/vite-custom/selectiveImport'
 
 createApp(App)
     .use(i18nPlugin, {
         availableLocales: [
-            {value: 'cs', icon: "twemoji:flag-czechia"},
-            {value: 'en', icon: "twemoji:flag-united-kingdom"},
+            { value: 'cs', icon: "twemoji:flag-czechia" },
+            { value: 'en', icon: "twemoji:flag-united-kingdom" },
         ],
     })
     .use(itPanelsVuePlugin, {
@@ -27,21 +28,9 @@ createApp(App)
         ],
 
         rootSpec: () => {
-            // TODO: Move this logic to a plugin through vite.config.ts
-            const name = __ROOT_PANEL_SPEC_MODULE__
-
-            const specFiles = import.meta.glob('../custom/**/*.rootSpec.@(js|ts)')
-            const entries = Object.entries(specFiles)
-                .map(([path, loadFn]) => [path.replace('../custom', '@custom'), loadFn])
-            const availableSpecs = Object.fromEntries(entries)
-            const importFn = availableSpecs[name] || availableSpecs[name + '.js'] || availableSpecs[name + '.ts']
-
-            if (!importFn) {
-                return Promise.reject(`App spec '${name}' not found`)
-            }
-
-            return importFn()
-                .then((module: Record<string, any>) => ({...module}))
+            const availableSpecs = mapImporters(import.meta.glob('../custom/**/*.rootSpec.@(js|ts)'))
+            const importFn = selectImporter(availableSpecs, __ROOT_PANEL_SPEC_MODULE__)
+            return importAsObj(importFn)
         },
     })
     .mount('#app')
