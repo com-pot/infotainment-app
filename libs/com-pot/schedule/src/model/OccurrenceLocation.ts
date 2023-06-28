5888cafa20503165c87e11e0217bf8f40819b6f0
Toanir <Thoronir42@Gmail.com>
2023-06-29 00:26:41 +0200

    Add json-schemas to models

--- libs/com-pot/schedule/src/model/OccurrenceLocation.ts
+++ libs/com-pot/schedule/src/model/OccurrenceLocation.ts
@@ -1,9 +1,25 @@
-import { Localized } from "@typeful/model/types/I18n"
+import { localizedType } from "@typeful/model/types/I18n"
+import { FromSchema } from "json-schema-to-ts"
 
-type OccurrenceLocationApiForm = {
-    id: string,
-    title: Localized<string>,
-}
+export const occurrenceLocationSchema = {
+    type: "object",
+    properties: {
+        id: {type: "string"},
+        title: localizedType({type: "string"} as const),
+
+        center: {
+            type: "object",
+            properties: {
+                latitude: {type: "number", minimum: -90, maximum: 90},
+                longitude: {type: "number", minimum: -180, maximum: 180},
+            },
+            required: ["latitude", "longitude"],
+        },
+    },
+    required: ["id", "title"],
+} as const
+
+type OccurrenceLocationApiForm = FromSchema<typeof occurrenceLocationSchema>
 
 export type OccurrenceLocation = {
     app: OccurrenceLocationApiForm,
--- libs/typeful-model/src/types/text.ts
+++ libs/typeful-model/src/types/text.ts
@@ -1,1 +1,17 @@
-export type TextContent = string | {html: string}
+import { FromSchema } from "json-schema-to-ts"
+
+export const textContentSchema = {
+    oneOf: [
+        {type: "string"},
+        {
+            type: "object",
+            properties: {
+                html: {type: "string"},
+            },
+            required: ["html"],
+            additionalProperties: false,
+        },
+    ],
+} as const
+
+export type TextContent = FromSchema<typeof textContentSchema>
