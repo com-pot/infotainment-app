f68f13b7e9cfa09f96fc16c2684876d2479c69b0
Toanir <Thoronir42@Gmail.com>
2023-10-25 12:50:58 +0200

    WIP: Update api structure to use compot backstage data

--- libs/com-pot/schedule/src/model/ProgramItemOccurrence.ts
+++ libs/com-pot/schedule/src/model/ProgramItemOccurrence.ts
@@ -1,30 +1,32 @@
 import { Localized, LocalizedTextContent } from "@typeful/model/types/I18n"
 import { OccurrenceLocation } from "./OccurrenceLocation"
-import { ProgramScheduleItem } from "./ProgramScheduleItem"
+import { Activity } from "./ProgramScheduleItem"
 import { FromSchema } from "json-schema-to-ts"
 
 export const programItemOccurenceSchema = {
     type: 'object',
     properties: {
-        item: {type: "string"},
-        time: {
-            type: 'object',
-            properties: {
-                start: {type: 'string', format: 'date'},
-                end: {type: 'string', format: 'date'},
-            },
-            additionalProperties: false,
-            required: ['start'],
-        },
+        activity: {type: "string"},
+        location: {type: "string"},
+        
+        day: {type: "number"},
+        start: {type: 'string', format: 'date'},
+        end: {type: 'string', format: 'date'},
+
+        params: {type: "object", additionalProperties: true},
     },
-    required: ['item', 'time'],
+    required: ['activity', 'day', 'start'],
+    additionalProperties: false,
 } as const
 
-export type ProgramItemOccurence = {
+export type ActivityOccurrence = {
     app: {
-        item: ProgramScheduleItem['app'],
+        activity: Activity['app'],
         location?: OccurrenceLocation['app'],
-        time: { start: Date, end?: Date },
+
+        day: number,
+        start: Date,
+        end?: Date,
 
         params?: Record<string, string|Localized<string>>,
         description?: LocalizedTextContent,
@@ -32,12 +34,19 @@ export type ProgramItemOccurence = {
     api: FromSchema<typeof programItemOccurenceSchema>,
 }
 
-export type OccurrenceItemRawData = {
-    item: string,
-    time: string[],
+export function groupOccurrencesByDay<T extends Pick<ActivityOccurrence["app"], "day" | "start" | "end">>(occurrences: T[]): T[][] {
+    const groups: Record<string, T[]> = {}
 
-    location?: string,
+    occurrences.forEach((occurrence) => {
+        if (!groups[occurrence.day]) groups[occurrence.day] = []
+        groups[occurrence.day].push(occurrence)
+    })
 
-    params?: Record<string, any>,
-    description?: LocalizedTextContent,
+    return Object.entries(groups)
+        .map(([day, group]) => [Number(day), group] as const)
+        .sort(([a], [b]) => a - b)
+        .map(([day, group]) => group.sort((a, b) => {
+            if (!a.start || !b.start) return 0
+            return a.start.getTime() - b.start.getTime()
+        }))
 }
