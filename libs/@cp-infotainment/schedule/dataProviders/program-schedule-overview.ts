8cefd9d001ee966e297be1fd574fd5484e0259a9
Toanir <Thoronir42@Gmail.com>
2023-10-27 10:54:10 +0200

    WIP: Adapt data source to use compot backend

--- libs/@cp-infotainment/schedule/dataProviders/program-schedule-overview.ts
+++ libs/@cp-infotainment/schedule/dataProviders/program-schedule-overview.ts
@@ -22,21 +22,32 @@ export default defineDataProvider<any, Args>({
         let now = args.now || new Date()
 
         const [
-            items,
+            activities,
             locations,
             occurrencesRaw,
         ] = await Promise.all([
-            this.api.req<ProgramScheduleItem['app'][]>('GET', 'com-pot/schedule/items'),
-            this.api.req<OccurrenceLocation['app'][]>('GET', 'com-pot/schedule/locations'),
-            this.api.req<OccurrenceItemRawData[][]>('GET', 'com-pot/schedule/occurrences-raw'),
+            this.api.req<{items: ProgramScheduleItem['app'][]}>('GET', 'backstage/typeful/collection/_compot_schedule__Activities/items?_perPage=100&meet=fhp'),
+            this.api.req<{items: OccurrenceLocation['app'][]}>('GET', 'backstage/typeful/collection/_compot_locations__Places/items?_perPage=100&meet=fhp'),
+            this.api.req<{items: OccurrencePersistedData[]}>('GET', 'backstage/typeful/collection/_compot_schedule__ActivityOccurrences/items?_perPage=150&meet=fhp'),
         ])
 
         const hydrator = createOccurrencesHydrator(new Date(args.from))
-        const groups = hydrator.hydrateOccurrences(occurrencesRaw, items, locations)
 
-        const result = groups.filter((group) => group.date.getTime() >= now.getTime())
+        const groups = hydrator.hydrateOccurrences(occurrencesRaw.items, activities.items, locations.items, now)
 
-        return result
+        const item = groups[0]?.items?.[0]
+        if (item) {
+            const refreshAt = item.time.end || item.time.start
+            if (refreshAt) {
+                const timeoutMs = refreshAt.getTime() - now.getTime()
+                console.log("refresh after", timeoutMs)
+                setTimeout(() => {
+                    window.location.reload()
+                }, timeoutMs)
+            }
+        }
+
+        return groups
     },
 })
 
@@ -66,20 +77,28 @@ function createOccurrencesHydrator(startDate: Date) {
 
     const createOccurrencesGroup = (
         group: OccurrenceItemRawData[], iDay: number,
-        scheduleItems: ProgramScheduleItem['app'][],
+        activities: ProgramScheduleItem['app'][],
         locations: OccurrenceLocation['app'][],
-    ): ProgramEntriesGroup => {
+        now?: Date,
+    ): ProgramEntriesGroup | null => {
         const itemsOrInvalid = group
             .map((occurrence: OccurrenceItemRawData, iOcc): ProgramEntriesGroup['items'][number]|null => {
-                const item = scheduleItems.find((item) => item.id === occurrence.item)!
+                const item = activities.find((item) => item.id === occurrence.item || item.slug === occurrence.item)!
                 const time = makeOccurenceTime(occurrence.time, iDay)
                 if (!item || !time) {
                     console.error(`Invalid occurrence spec '${iDay}:${iOcc}'`, occurrence)
                     return null
                 }
-                const location = occurrence.location ? locations.find((location) => location.id === occurrence.location) : undefined
+                const location = occurrence.location ? locations.find((location) => location.id === occurrence.location || location.slug === occurrence.location) : undefined
                 if (occurrence.location && !location) {
                     console.warn(`Occurrence location '${occurrence.location}' not found in '${iDay}:${iOcc}'`);
+                }
+
+                if (now) {
+                    const showUntil = time.end || time.start
+                    if (showUntil < now) {
+                        return null
+                    }
                 }
 
                 return {
@@ -92,17 +111,61 @@ function createOccurrencesHydrator(startDate: Date) {
         const items = itemsOrInvalid
             .filter(Boolean) as NonNullable<typeof itemsOrInvalid[number]>[]
 
+        if (!items.length) {
+            return null
+        }
+
         const date = new Date(items[0].time.start)
-        date.setHours(0, 0, 0, 0)
+        date.setHours(24, 0, 0, -1)
 
         return { date, items }
     }
 
-    const hydrateOccurrences = (scheduleOccurencesGroupedByDay: OccurrenceItemRawData[][], scheduleItems: ProgramScheduleItem['app'][], locations: OccurrenceLocation['app'][]): ProgramEntriesGroup[] => {
-        return scheduleOccurencesGroupedByDay.map((group, iDay) => createOccurrencesGroup(group, iDay, scheduleItems, locations))
+    const hydrateOccurrences = (
+        scheduleOccurences: OccurrencePersistedData[],
+        activities: ProgramScheduleItem['app'][],
+        locations: OccurrenceLocation['app'][],
+        now?: Date,
+    ): ProgramEntriesGroup[] => {
+        const scheduleOccurencesGroupedByDay = groupOccurrencesByDay(scheduleOccurences)
+        return scheduleOccurencesGroupedByDay
+            .map((group, iDay) => createOccurrencesGroup(group, iDay, activities, locations, now))
+            .filter((item): item is NonNullable<typeof item> => !!item)
     }
 
     return {
         hydrateOccurrences,
     }
+}
+
+type OccurrencePersistedData = {
+    activity: OccurrenceItemRawData["item"],
+    location: OccurrenceItemRawData["location"],
+    day: number,
+    start: OccurrenceItemRawData["time"][0],
+    end: OccurrenceItemRawData["time"][1],
+
+    params: OccurrenceItemRawData["params"],
+}
+
+function groupOccurrencesByDay(occurrences: OccurrencePersistedData[]) {
+    const groups: Record<string, OccurrenceItemRawData[]> = {}
+
+    occurrences.forEach((occurrence) => {
+        if (!groups[occurrence.day]) groups[occurrence.day] = []
+        groups[occurrence.day].push({
+            item: occurrence.activity,
+            location: occurrence.location,
+            time: [occurrence.start, occurrence.end],
+            params: occurrence.params,
+        })
+    })
+
+    return Object.entries(groups)
+        .map(([day, group]) => [Number(day), group] as const)
+        .sort(([a], [b]) => a - b)
+        .map(([day, group]) => group.sort((a, b) => {
+            if (!a.time[0] || !b.time) return 0
+            return a.time[0].localeCompare(b.time[0])
+        }))
 }
