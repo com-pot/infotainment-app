351faf5cf582ac710eaabe23326e6cc6067bb04a
Toanir <Thoronir42@Gmail.com>
2024-05-01 10:10:56 +0200

    Flatten model and update terminology

---
+++ libs/com-pot/schedule/src/model/ActivityOccurrence.ts
@@ -0,0 +1,52 @@
+import { Localized, LocalizedTextContent } from "@typeful/model/types/I18n"
+import { OccurrenceLocation } from "./OccurrenceLocation"
+import { Activity } from "./Activity"
+import { FromSchema } from "json-schema-to-ts"
+
+export const activityOccurenceSchema = {
+    type: 'object',
+    properties: {
+        activity: {type: "string"},
+        location: {type: "string"},
+        
+        day: {type: "number"},
+        start: {type: 'string', format: 'date'},
+        end: {type: 'string', format: 'date'},
+
+        params: {type: "object", additionalProperties: true},
+    },
+    required: ['activity', 'day', 'start'],
+    additionalProperties: false,
+} as const
+
+export type ActivityOccurrence = {
+    app: {
+        activity: Activity['app'],
+        location?: OccurrenceLocation['app'],
+
+        day: number,
+        start: Date,
+        end?: Date,
+
+        params?: Record<string, string|Localized<string>>,
+        description?: LocalizedTextContent,
+    },
+    api: FromSchema<typeof activityOccurenceSchema>,
+}
+
+export function groupOccurrencesByDay<T extends Pick<ActivityOccurrence["app"], "day" | "start" | "end">>(occurrences: T[]): T[][] {
+    const groups: Record<string, T[]> = {}
+
+    occurrences.forEach((occurrence) => {
+        if (!groups[occurrence.day]) groups[occurrence.day] = []
+        groups[occurrence.day].push(occurrence)
+    })
+
+    return Object.entries(groups)
+        .map(([day, group]) => [Number(day), group] as const)
+        .sort(([a], [b]) => a - b)
+        .map(([day, group]) => group.sort((a, b) => {
+            if (!a.start || !b.start) return 0
+            return a.start.getTime() - b.start.getTime()
+        }))
+}
